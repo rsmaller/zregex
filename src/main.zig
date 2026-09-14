62b4c8371530a9e9726c3b0b48e0a1425f7a98ed
ryan <ryjobobo@gmail.com>
2026-09-14 19:06:24 -0400

    Updated zig src to 0.16.0

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 const zregex = @import("zregex");
 
-pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+pub fn main(init: std.process.Init) !void {
+    var gpa = std.heap.DebugAllocator(.{}){};
     const allocator = gpa.allocator();
     defer {
         if (gpa.deinit() != .ok) {
@@ -10,7 +10,7 @@ pub fn main() !void {
         }
     }
     var stdout_buffer: [1024]u8 = undefined;
-    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buffer);
     const stdout = &stdout_writer.interface;
     const string_to_match = "john.doe@gmail.com";
     // const pattern: []const u8 = "(?<=abc|ab)(?<name1>hiii)(|a|b|c|a|)\\ba \\B [\\q-\\z]^\\[\\*\\..(?>abc)\\n(|)(?=\\s{3,}+|)(?!\\s{3,}+|)(?<=az)[^\\t-\\n](?<!az)[abc]+?-(|\\d{,5})-(\\d{,}|-\\d{15})$";
@@ -31,3 +31,4 @@ pub fn main() !void {
     _ = myMatch;
     try stdout.flush(); // Don't forget to flush!
 }
+
